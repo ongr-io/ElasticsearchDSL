fc3b1297d632ea418698cd026a60aeb92a36bbc0
Martynas Sudintas <martynas.sudintas@ongr.io>
2015-02-23 14:36:07 +0200

    configuration mappings are now able to accept separate documents

--- Filter/NestedFilter.php
+++ Filter/NestedFilter.php
@@ -32,11 +32,6 @@ class NestedFilter implements BuilderInterface
     private $query;
 
     /**
-     * @var array
-     */
-    private $parameters;
-
-    /**
      * @param string           $path
      * @param BuilderInterface $query
      * @param array            $parameters
