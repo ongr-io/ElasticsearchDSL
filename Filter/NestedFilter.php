4b2cab9e34a12cd230f62bcd4d15d5afae7044f2
Linas Mockus <linas.mockus@ongr.io>
2015-02-23 09:51:29 +0200

    Added NestedFilter.

---
+++ Filter/NestedFilter.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\ElasticsearchBundle\DSL\Filter;
+
+use ONGR\ElasticsearchBundle\DSL\BuilderInterface;
+use ONGR\ElasticsearchBundle\DSL\ParametersTrait;
+
+/**
+ * Nested filter implementation.
+ */
+class NestedFilter implements BuilderInterface
+{
+    use ParametersTrait;
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var BuilderInterface
+     */
+    private $query;
+
+    /**
+     * @var array
+     */
+    private $parameters;
+
+    /**
+     * @param string           $path
+     * @param BuilderInterface $query
+     * @param array            $parameters
+     */
+    public function __construct($path, BuilderInterface $query, array $parameters = [])
+    {
+        $this->path = $path;
+        $this->query = $query;
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return 'nested';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toArray()
+    {
+        $query = [
+            'path' => $this->path,
+            'filter' => [
+                $this->query->getType() => $this->query->toArray(),
+            ],
+        ];
+
+        $output = $this->processArray($query);
+
+        return $output;
+    }
+}
