6769deeff8338831dc211106dc770bf84f6bd15f
Mantas Jonušas <mantas.jonusas@nfq.lt>
2015-03-23 10:37:19 +0200

    Added IPv4 aggregation

---
+++ Aggregation/Ipv4RangeAggregationTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\ElasticsearchBundle\Tests\Unit\DSL\Aggregation;
+
+use ONGR\ElasticsearchBundle\DSL\Aggregation\Ipv4RangeAggregation;
+
+class Ipv4RangeAggregationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test exception when field and range are not set.
+     *
+     * @expectedException \LogicException
+     */
+    public function testIfExceptionIsThrownWhenFieldAndRangeAreNotSet()
+    {
+        $agg = new Ipv4RangeAggregation('foo');
+        $agg->toArray();
+    }
+}
