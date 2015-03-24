b65d08a1473cb2170744e1a2f7429f377cf6c6c2
Mantas Simkus <mantas.simkus@ongr.io>
2015-03-24 10:53:35 +0200

    Added Unit/DSL/Highlight tests

--- Highlight/FieldTest.php
+++ Highlight/FieldTest.php
@@ -9,8 +9,9 @@
  * file that was distributed with this source code.
  */
 
-namespace ONGR\ElasticsearchBundle\Tests\Unit\DSL\Aggregation;
+namespace ONGR\ElasticsearchBundle\Tests\Unit\DSL\Highlight;
 
+use ONGR\ElasticsearchBundle\DSL\Filter\TermFilter;
 use ONGR\ElasticsearchBundle\DSL\Highlight\Field;
 
 /**
@@ -38,5 +39,42 @@ class FieldTest extends \PHPUnit_Framework_TestCase
 
         $field->setHighlighterType('wrongValue');
         $this->assertEquals($initValue, $field->getType());
+    }
+
+    /**
+     * Tests toArray method.
+     */
+    public function testFieldToArray()
+    {
+        $field = new Field('test');
+        $field->setFragmentSize(5);
+        $field->setNumberOfFragments(5);
+        $field->setHighlightQuery(new TermFilter('key1', 'value1'));
+        $field->setNoMatchSize(3);
+        $field->setForceSource(true);
+
+        $result = [
+            'fragment_size' => 5,
+            'number_of_fragments' => 5,
+            'matched_fields' => ['test'],
+            'highlight_query' => [
+                'term' => [
+                    'key1' => 'value1',
+                ],
+            ],
+            'no_match_size' => 3,
+            'force_source' => true,
+        ];
+        $this->assertEquals($result, $field->toArray());
+    }
+
+    /**
+     * Tests getName method.
+     */
+    public function testFieldGetName()
+    {
+        $field = new Field('test');
+        $result = $field->getName();
+        $this->assertEquals('test', $result);
     }
 }
