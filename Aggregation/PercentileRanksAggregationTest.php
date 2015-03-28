e4571a83011959ce0b16788e59599d42f03f8917
Mantas Jonušas <mantas.jonusas@nfq.lt>
2015-03-28 11:12:09 +0200

    Refactored percentile ranks aggregation

--- Aggregation/PercentileRanksAggregationTest.php
+++ Aggregation/PercentileRanksAggregationTest.php
@@ -13,16 +13,53 @@ namespace ONGR\ElasticsearchBundle\Tests\Unit\DSL\Aggregation;
 
 use ONGR\ElasticsearchBundle\DSL\Aggregation\PercentileRanksAggregation;
 
+/**
+ * Percentile ranks aggregation unit tests.
+ */
 class PercentileRanksAggregationTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Tests if exception is thrown.
+     * @var PercentileRanksAggregation
+     */
+    public $agg;
+
+    /**
+     * Phpunit setup.
+     */
+    public function setUp()
+    {
+        $this->agg = new PercentileRanksAggregation('foo');
+    }
+
+    /**
+     * Tests if exception is thrown when required parameters not set.
      *
      * @expectedException \LogicException
      */
     public function testIfPercentileRanksAggregationThrowsAnException()
     {
-        $agg = new PercentileRanksAggregation('foo');
-        $agg->toArray();
+        $this->agg->toArray();
+    }
+
+    /**
+     * Tests exception when only field is set.
+     *
+     * @expectedException \LogicException
+     */
+    public function testIfExceptionIsThrownWhenFieldSetAndValueNotSet()
+    {
+        $this->agg->setField('bar');
+        $this->agg->toArray();
+    }
+
+    /**
+     * Tests exception when only value is set.
+     *
+     * @expectedException \LogicException
+     */
+    public function testIfExceptionIsThrownWhenScriptSetAndValueNotSet()
+    {
+        $this->agg->setScript('bar');
+        $this->agg->toArray();
     }
 }
