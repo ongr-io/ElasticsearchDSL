f283306e0008179d49fbb34dacc94b59dad6d09c
Mantas Jonušas <mantas.jonusas@nfq.lt>
2015-04-09 10:08:12 +0300

    Updated BuilderInterface toArray return type

--- BuilderInterface.php
+++ BuilderInterface.php
@@ -19,7 +19,7 @@ interface BuilderInterface
     /**
      * Generates array which will be passed to elasticsearch-php client.
      *
-     * @return array
+     * @return array|object
      */
     public function toArray();
 
