eea2794155fd0df615e9f81c88b1fa6d4c6a8c65
Martynas Sudintas <martynas.sudintas@ongr.io>
2015-04-29 13:40:03 +0300

    improved NotFilter with setters

--- Filter/NotFilter.php
+++ Filter/NotFilter.php
@@ -16,6 +16,8 @@ use ONGR\ElasticsearchBundle\DSL\ParametersTrait;
 
 /**
  * Represents Elasticsearch "not" filter.
+ *
+ * @link http://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-not-filter.html
  */
 class NotFilter implements BuilderInterface
 {
@@ -30,10 +32,32 @@ class NotFilter implements BuilderInterface
      * @param BuilderInterface $filter     Filter.
      * @param array            $parameters Optional parameters.
      */
-    public function __construct($filter, array $parameters = [])
+    public function __construct(BuilderInterface $filter = null, array $parameters = [])
+    {
+        if ($filter !== null) {
+            $this->setFilter($filter);
+        }
+        $this->setParameters($parameters);
+    }
+    
+    /**
+     * Returns filter.
+     *
+     * @return BuilderInterface
+     */
+    public function getFilter()
+    {
+        return $this->filter;
+    }
+
+    /**
+     * Sets filter.
+     *
+     * @param BuilderInterface $filter
+     */
+    public function setFilter(BuilderInterface $filter)
     {
         $this->filter = $filter;
-        $this->setParameters($parameters);
     }
 
     /**
