773a65319edd23c4feb22c74a78d67b31b1f9de3
Mantas Urnieža <mantas.urnieza@nfq.lt>
2015-06-10 10:52:37 +0300

    FLT query implementation

---
+++ Query/FuzzyLikeThisQuery.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\ElasticsearchBundle\DSL\Query;
+
+/**
+ * Elasticsearch fuzzy_like_this query class.
+ */
+class FuzzyLikeThisQuery extends FuzzyLikeThisFieldQuery
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return 'fuzzy_like_this';
+    }
+}
