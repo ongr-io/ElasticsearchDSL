7d192550f622e2023fd9ab2ea00511c112461f80
Stanislav <vitvitskiy@pdffiller.com>
2019-04-08 08:54:17 +0300

    Adding param support cardinality aggregation

---
+++ src/Aggregation/Metric/CountCardinalityAggregation.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace ONGR\ElasticsearchDSL\Aggregation\Metric;
+
+use ONGR\ElasticsearchDSL\Aggregation\AbstractAggregation;
+
+/**
+ * Class CountCardinalityAggregation
+ * @package ONGR\ElasticsearchDSL\Aggregation\Metric
+ */
+class CountCardinalityAggregation extends AbstractAggregation
+{
+    /**
+     * CountAggregation constructor.
+     * @param $name
+     * @param null $field
+     */
+    public function __construct($name, $field = null)
+    {
+        parent::__construct($name);
+
+        $this->setField($field);
+    }
+
+    /**
+     * @return array|\stdClass
+     */
+    protected function getArray()
+    {
+        $data = array_filter(
+            [
+                'field' => $this->getField(),
+            ]
+        );
+
+        return $data;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function supportsNesting()
+    {
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return 'cardinality';
+    }
+}
