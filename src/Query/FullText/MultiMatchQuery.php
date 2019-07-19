2bcaa125366d86547b081e3a70bb1499502fe145
John Fitzpatrick <j13k@users.noreply.github.com>
2019-07-19 16:45:20 +1000

    Update to allow multi-match query with no fields. (#291)
    From the Elasticsearch documentation:
    
    If no fields are provided, the multi_match query defaults to the
    `index.query.default_field` index settings, which in turn defaults to
    `*`. `*` extracts all fields in the mapping that are eligible to term
    queries and filters the metadata fields. All extracted fields are then
    combined to build a query.

--- src/Query/FullText/MultiMatchQuery.php
+++ src/Query/FullText/MultiMatchQuery.php
@@ -18,6 +18,12 @@ use ONGR\ElasticsearchDSL\ParametersTrait;
  * Represents Elasticsearch "multi_match" query.
  *
  * @link https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html
+ *
+ * Allows `$fields` to be an empty array to represent 'no fields'. From the Elasticsearch documentation:
+ *
+ * If no fields are provided, the multi_match query defaults to the `index.query.default_field` index settings,
+ * which in turn defaults to `*`. `*` extracts all fields in the mapping that are eligible to term queries and filters
+ * the metadata fields. All extracted fields are then combined to build a query.
  */
 class MultiMatchQuery implements BuilderInterface
 {
@@ -59,9 +65,11 @@ class MultiMatchQuery implements BuilderInterface
     public function toArray()
     {
         $query = [
-            'fields' => $this->fields,
             'query' => $this->query,
         ];
+        if (count($this->fields)) {
+            $query['fields'] = $this->fields;
+        }
 
         $output = $this->processArray($query);
 
